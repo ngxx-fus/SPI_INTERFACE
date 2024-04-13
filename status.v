02467775917b99cf1e82cda2b677221bed059554
ngxx-fus <msnp@outlook.com.vn>
2024-04-13 18:00:07 +0700

    V.3.0

--- status.v
+++ status.v
@@ -1,14 +1,21 @@
 module STATUS_COMBINATION (
-	input SENDER_REG_FULL,
-	input SENDER_REG_EMPTY,
-    input RECEIVER_REG_FULL,
-    input RECEIVER_REG_EMPTY,
-    input S_CLK,
+	input SENDER_BUFFER_FULL_STATE,
+	input SENDER_FULL_STATE,
+    input SENDER_EMPTY_STATE,
+
+    input RECEIVER_BUFFER_FULL_STATE,
+    input RECEIVER_FULL_STATE,
+    input RECEIVER_EMPTY_STATE,
+
     input SENDER_WRITE,
+    input RECEIVER_READ,
+
+    input S_CLK,
     input CLR,
+    
     output [7:0]STATUS
 );
-
+    //TODO: làm cái bảng này như trong bảng trong sách 
     reg [7:0] LOCAL_STATUS ;
     wire HIGH, LOW;
     initial begin
@@ -18,11 +25,10 @@ module STATUS_COMBINATION (
     always @(CLR) begin
         LOCAL_STATUS = 8'h30;
     end
-
     //LOCAL_STATUS[2]
     always @(posedge S_CLK)
     begin
-        if(RECEIVER_REG_FULL == HIGH)
+        if(RECEIVER_FULL_STATE == HIGH)
             LOCAL_STATUS[2] = HIGH;
         else
             LOCAL_STATUS[2] = LOW;
@@ -32,7 +38,7 @@ module STATUS_COMBINATION (
     // LOCAL_STATUS [3]
     always @(posedge S_CLK)//RESET LOCAL_STATUS[3] when CLEAR 
     begin   
-	    if (SENDER_REG_EMPTY == LOW && SENDER_WRITE == HIGH)//SENDER_REG_EMPTY <> HIGH & WRITE
+	    if (SENDER_EMPTY_STATE == LOW && SENDER_WRITE == HIGH)//SENDER_EMPTY_STATE <> HIGH & WRITE
             LOCAL_STATUS[3] = HIGH;
         else if (CLR == HIGH)
             LOCAL_STATUS[3] = LOW;
@@ -41,23 +47,23 @@ module STATUS_COMBINATION (
     end
 
     //  LOCAL_STATUS[4]
-    //  SENDER_REG_EMPTY
-    always @ (SENDER_REG_EMPTY)
+    //  SENDER_EMPTY_STATE
+    always @ (SENDER_EMPTY_STATE)
     begin
-         LOCAL_STATUS [4] = SENDER_REG_EMPTY;
+         LOCAL_STATUS [4] = SENDER_EMPTY_STATE;
     end
     //  LOCAL_STATUS[5]
-    always @ (SENDER_REG_EMPTY)
+    always @ (SENDER_EMPTY_STATE)
     begin
-        if (SENDER_REG_EMPTY == HIGH)
+        if (SENDER_EMPTY_STATE == HIGH)
             LOCAL_STATUS [5] = HIGH;
         else
             LOCAL_STATUS [5] = LOW;
     end
     //  LOCAL_STATUS[6]
-    always @ (RECEIVER_REG_FULL)
+    always @ (RECEIVER_FULL_STATE)
     begin
-	    LOCAL_STATUS [6] = ~RECEIVER_REG_EMPTY  ;
+	    LOCAL_STATUS [6] = ~RECEIVER_EMPTY_STATE  ;
     end
 
     assign STATUS = LOCAL_STATUS;
