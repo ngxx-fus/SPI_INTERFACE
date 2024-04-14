77af725d7aea10ec9560ede5328592079ec61ed8
ngxx-fus <msnp@outlook.com.vn>
2024-04-14 19:44:56 +0700

    v.3.7

--- d_ff.v
+++ d_ff.v
@@ -4,15 +4,14 @@ module d_ff(output reg Q, input DATA, input CLK, input CLR, input PRE);
     initial begin
         Q = 1'b0;
         end
-    always @(CLK, CLR, PRE)
+    always @(posedge CLK, posedge CLR, posedge PRE)
         begin
-            if( CLR == 1'b0 && PRE == 1'b0)
-                Q = (CLK == 1)?DATA:Q;
-            else if(CLR == 1'b1 && PRE == 1'b0)
-                #5 Q = 1'b0;
-            else if(CLR == 1'b0 && PRE == 1'b1)
-                #5 Q = 1'b1;
-            else #5 Q = Q;
+				if(CLR == 1) Q = 1'b0;
+				else if(PRE == 1) Q = 1'b1;
+                else if(PRE == 0 && CLR == 0) Q = DATA;
+                else Q = Q;
+
         end
+
 endmodule
 
