4c42521bc7a0dbabd77296a34d81601879bac68c
fus <msnp@outlook.com.vn>
2024-04-19 14:21:33 +0700

    v.4.9 - Update clk_gen.v

--- clk_gen.v
+++ clk_gen.v
@@ -1,19 +1,18 @@
 `timescale 1ns/1ps
 module CLK_GEN(
-    //PERIOD = FACTOR*(10ns)
-    input [31:0] FACTOR,
-    output CLK
+    input [31:0] FACTOR,  output reg CLK
 );
     reg [7:0] c;
     reg [32:0] temp;
     initial begin
+        CLK = 0;
         for( c = 0; c < 8'hFF; c=c+8'h01)
             for( temp = FACTOR; temp != 32'h0000; temp = temp - 32'h0001)
 				begin
-                #1;
+                #1; 
+                CLK =~CLK;
 					 //temp = (temp == 32'h0000)?(FACTOR):(temp);
 				end
     end
 
-    assign CLK = c[0];
 endmodule
